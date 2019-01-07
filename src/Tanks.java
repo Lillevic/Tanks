2f9e1a1d765b17582a22a1ee3bfe8e8a5fe8ed5e
Lillevic <victorravysetornhoj@gmail.com>
2019-01-07 12:57:17 +0100

    Initial commit

---
+++ src/Tanks.java
@@ -0,0 +1,55 @@
+package Tanks;
+
+import processing.core.*;
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import shiffman.box2d.*; 
+import org.jbox2d.common.*; 
+import org.jbox2d.dynamics.joints.*; 
+import org.jbox2d.collision.shapes.*; 
+import org.jbox2d.collision.shapes.Shape; 
+import org.jbox2d.common.*; 
+import org.jbox2d.dynamics.*; 
+import org.jbox2d.dynamics.contacts.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Tanks extends PApplet {
+  public void setup() {
+
+
+
+
+
+
+
+
+
+
+
+// A reference to our box2d world
+Box2DProcessing box2d;
+
+// A list we'll use to track fixed objects
+//ArrayList<Boundary> boundaries;
+    noLoop();
+  }
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Tanks.Tanks" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
